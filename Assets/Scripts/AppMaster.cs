0986919ef0421dc1cbd91508953a762a8d8c6b18
jonwho <housuilun@yahoo.com>
2015-02-12 22:15:36 -0800

    add singleton class

---
+++ Assets/Scripts/AppMaster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppMaster : MonoBehaviour {
+
+	public static AppMaster AM;
+
+	// access this anywhere using AppMaster.AM.name
+	public string name = "augmon";
+
+
+	void Awake() {
+		if (AM != null) {
+			GameObject.Destroy (AM);
+		} else {
+			AM = this;
+		}
+
+		DontDestroyOnLoad (this);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
