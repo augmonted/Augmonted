d38016e61a4ce1ee29e0439529198f6fc642c80d
jonwho <housuilun@yahoo.com>
2015-02-17 11:47:30 -0800

    remove uifb stuff

--- Assets/Scripts/FBHolder.cs
+++ Assets/Scripts/FBHolder.cs
@@ -4,20 +4,10 @@ using System.Collections;
 
 public class FBHolder : MonoBehaviour {
 	
-	public GameObject UIFBIsLoggedIn;
-	public GameObject UIFBIsNotLoggedIn;
-	public GameObject UIFBAvatar;
-	public GameObject UIFBUsername;
-	
 	// Initialization
 	void Awake()
 	{
 		FB.Init (SetInit, OnHideUnity);
-
-		// preserve these GameObjects across scene changes
-		DontDestroyOnLoad (UIFBAvatar);
-		DontDestroyOnLoad (UIFBIsLoggedIn);
-		DontDestroyOnLoad (UIFBUsername);
 	}
 	
 	//-------------------------------------------------------------------------
@@ -29,12 +19,10 @@ public class FBHolder : MonoBehaviour {
 		// check if user is already logged in
 		if (FB.IsLoggedIn)
 		{
-			DealWithFBMenus (true);
 			Debug.Log ("FB Logged In");
 		}
 		else
 		{
-			DealWithFBMenus (false);
 		}
 	}
 	
@@ -63,55 +51,11 @@ public class FBHolder : MonoBehaviour {
 	{
 		if(FB.IsLoggedIn)
 		{
-			DealWithFBMenus (true);
 			Debug.Log ("FB Login Worked");
 		}
 		else
 		{
-			DealWithFBMenus (false);
 			Debug.Log ("FB Login Failed");
-		}
-	}
-	
-	//-------------------------------------------------------------------------
-	void DealWithFBMenus(bool isLoggedIn)
-	{
-		if(isLoggedIn)
-		{
-			UIFBIsLoggedIn.SetActive (true);
-			UIFBIsNotLoggedIn.SetActive (false);
-			
-			// Get profile picture
-			FB.API (Util.GetPictureURL("me", 128, 128), Facebook.HttpMethod.GET, DealWithProfilePicture);
-			// Get username
-
-			// change scene
-			Application.LoadLevel("user");
-		}
-		else
-		{
-			UIFBIsLoggedIn.SetActive (false);
-			UIFBIsNotLoggedIn.SetActive (true);
-		}
-	}
-	
-	//-------------------------------------------------------------------------
-	void DealWithProfilePicture(FBResult result)
-	{
-		// Check for errors when getting profile picture
-		if(result.Error != null)
-		{
-			Debug.Log("Problem with getting profile picture");
-			
-			// Try to get the picture again
-			FB.API (Util.GetPictureURL("me", 128, 128), Facebook.HttpMethod.GET, DealWithProfilePicture);
-			return;
-		}
-		else
-		{
-			Debug.Log ("Got profile picture");
-			UnityEngine.UI.Image userAvatar = UIFBAvatar.GetComponent<UnityEngine.UI.Image> ();
-			userAvatar.sprite = Sprite.Create (result.Texture, new Rect(0, 0, 128, 128), new Vector2(0, 0));
 		}
 	}
 }
