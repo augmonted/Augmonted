1642bdce769dd610fdf640d8dd2fcf560aa31e29
jonwho <housuilun@yahoo.com>
2015-02-20 14:39:18 -0800

    logic for back button up

--- Assets/Scripts/AppMaster.cs
+++ Assets/Scripts/AppMaster.cs
@@ -1,22 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * Singleton object for each scene. Should be useful
+ * for reusable methods we want.
+ * e.g. the back button
+ */
+
 public class AppMaster : MonoBehaviour {
 
-	public static AppMaster AM;
+	public static AppMaster instance = null;
 
 	// access this anywhere using AppMaster.AM.name
 	public string name = "augmon";
-
+	
+	public static AppMaster Instance() {
+		return instance;
+	}
 
 	void Awake() {
-		if (AM != null) {
-			GameObject.Destroy (AM);
-		} else {
-			AM = this;
-		}
-
-		DontDestroyOnLoad (this);
+		Debug.Log("AppMaster: Awake");
+		
+		if (instance == null)
+			instance = this;
+		
+		// keep the gameObject otherwise this
+		// instance won't exist
+		DontDestroyOnLoad (gameObject);
 	}
 
 	// Use this for initialization
@@ -26,6 +36,21 @@ public class AppMaster : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// if running on Android
+		if (Application.platform == RuntimePlatform.Android)
+		{
+			// if back button is pressed
+			if (Input.GetKeyDown(KeyCode.Escape)) { 
+				// if AR scene go back to user scene
+				if(Application.loadedLevelName == "main")
+					Application.LoadLevel("user");
+				// if user scene kill the app
+				if(Application.loadedLevelName == "user")
+					Application.Quit();
+				// if login scene kill the app
+				if(Application.loadedLevelName == "login")
+					Application.Quit();
+			}
+		}
 	}
 }
