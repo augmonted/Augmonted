0a2ec1c2732f1b95790c4b3c5e411583a7c11308
jonwho <housuilun@yahoo.com>
2015-02-26 13:40:07 -0800

    mobile build working except for ar scene

--- Assets/Editor/QCAR/Scripts/ExtensionImport.cs
+++ Assets/Editor/QCAR/Scripts/ExtensionImport.cs
@@ -30,6 +30,23 @@ namespace Vuforia.EditorClasses
                         Debug.Log("Setting Android target device to ARMv7");
                         PlayerSettings.Android.targetDevice = AndroidTargetDevice.ARMv7;
                     }
+// TODO: replace this with something like #if INCLUDE_METAL once Unity adds this
+// not sure if Unity 4.7+ will ever exist, but better safe than sorry
+#if UNITY_5_0 || UNITY_4_9 || UNITY_4_8 || UNITY_4_7 || (UNITY_4_6 && !UNITY_4_6_1 && !UNITY_4_6_2)
+                    // check if Graphics API for iOS is set to Metal or Universal
+                    if ((PlayerSettings.targetIOSGraphics == TargetIOSGraphics.Automatic) ||
+                        (PlayerSettings.targetIOSGraphics == TargetIOSGraphics.Metal))
+                    {
+                        Debug.Log("Setting iOS Graphics API to OpenGL ES 2.0, Vuforia does not support Metal yet.");
+                        PlayerSettings.targetIOSGraphics = TargetIOSGraphics.OpenGLES_2_0;
+                    
+                        if (PlayerSettings.targetIOSGraphics != TargetIOSGraphics.OpenGLES_2_0)
+                        {
+                            Debug.LogWarning("Failed to set iOS Graphics API to OpenGL ES 2.0. Please make sure to set this manually in the "+
+                                             "player settings, Vuforia does not support the Metal graphics API yet.");
+                        }
+                    }
+#endif
 
 #if INCLUDE_IL2CPP
 #if UNITY_5_0
